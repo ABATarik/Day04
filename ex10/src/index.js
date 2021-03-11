3a4452b44420c7813bb3c1ece030369ace0fdeb9
ABATarik <abaspahic.tarik@outlook.com>
2021-03-11 22:28:17 +0100

    Declare stirng Variable

---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+    var myFirstName = "Tarik";
+    var myLastName = "Abaspahic";
+    var myHomeTown = "Sarajevo";
+
+    
+        return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    
+        }
+    }
+    
+    console.log(main());
+    
+    module.exports = main;
