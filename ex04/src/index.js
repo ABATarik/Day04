1ab1483e4806d529710cc5c958afd8915f5e9c28
ABATarik <abaspahic.tarik@outlook.com>
2021-03-11 21:15:47 +0100

    Decimal numbers

---
+++ ex04/src/index.js
@@ -0,0 +1,19 @@
+function main(){
+var myDecimal = 2.54;
+var myDecimalE 
+
+myDecimalE = myDecimal * 10**-3;
+
+
+
+return{
+myDecimal,
+myDecimalE
+
+};
+}
+    
+console.log (main());
+
+
+modul.exports=main;
